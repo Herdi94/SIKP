2ac938daa4f3a89595bed424711dc8428debaaa8
herdi <dfikr94@gmail.com>
2017-01-30 18:04:13 +0700

    Update Welcome.php

--- application/controllers/pengaju/Welcome.php
+++ application/controllers/pengaju/Welcome.php
@@ -28,13 +28,13 @@ class Welcome extends CI_Controller
     {
         $this->load->library('email');
 
-/*
+
         $config['protocol'] = 'sendmail';
         $config['mailpath'] = '/usr/sbin/sendmail';
         $config['charset'] = 'iso-8859-1';
         $config['wordwrap'] = TRUE;
         $this->email->initialize($config);
-*/
+
     }
 
 
